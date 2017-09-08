4bbcd997a375dfa8e49d8eee502a460d0df2e796
Janardhan <janardhan.v@hp.com>
2017-09-08 11:05:04 +0530

    EServicesdemo

---
+++ src/test/java/com/hp/TestProject/SearchProduct.java
@@ -0,0 +1,32 @@
+package com.hp.TestProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SearchProduct {
+
+public WebDriver driver;
+	
+	@Test
+	public void launchBrowser() throws InterruptedException  {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\vj\\Downloads\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		/** Login to PFP */
+		driver.get("http://partner-itg.hp.com/");
+		driver.findElement(By.id("USER")).sendKeys("janardhan.v@hp.com");
+		driver.findElement(By.id("PASSWORD")).sendKeys("!!janu98");
+		driver.findElement(By.id("sign-in-btn")).click();
+		Thread.sleep(5000);
+		System.out.println("Current URL is "+driver.getCurrentUrl());
+		driver.findElement(By.xpath(".//*[@id='dock-content']/div/ul/li[3]/a")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Sign Out')]")).click();
+		//driver.quit();
+	}
+}
